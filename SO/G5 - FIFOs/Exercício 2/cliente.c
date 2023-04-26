1cecf1586076dd6ce90d0425bce4975047feeaef
João Pedro Baptista <102752884+joaobaptista03@users.noreply.github.com>
2023-04-26 20:43:52 +0100

    Add files via upload

---
+++ SO/G5 - FIFOs/Exercício 2/cliente.c
@@ -0,0 +1,19 @@
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define BUFSIZE 1024
+
+int main(int argc, char **argv) {
+    int fifo = open("fifo", O_WRONLY);
+    if (fifo < 0) {
+        write(2, "Error opening fifo.\n", 21);
+        exit(EXIT_FAILURE);
+    }
+
+    char buf[BUFSIZE]; int bytes;
+    while ((bytes = read(0, buf, BUFSIZE)) > 0) {
+        write(fifo, buf, bytes);
+    }
+}
---
+++ SO/G5 - FIFOs/Exercício 2/servidor.c
@@ -0,0 +1,30 @@
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define BUFSIZE 1024
+
+int main(int argc, char **argv) {
+    if (mkfifo("fifo", 0666) < 0) {
+        write(2, "Error creating fifo.\n", 22);
+        exit(EXIT_FAILURE);
+    }
+
+    int file = open("test.txt", O_WRONLY);
+    if (file < 0) {
+        write(2, "Error opening file.\n", 21);
+        exit(EXIT_FAILURE);
+    }
+    
+    int fifo = open("fifo", O_RDONLY);
+    if (fifo < 0) {
+        write(2, "Error opening fifo.\n", 21);
+        exit(EXIT_FAILURE);
+    }
+
+    char buf[BUFSIZE]; int bytes;
+    while ((bytes = read(fifo, buf, BUFSIZE)) > 0) {
+        write(file, buf, bytes);
+    }
+}
