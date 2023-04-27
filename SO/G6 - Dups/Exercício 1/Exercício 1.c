705dd4da2bf5754df0d293d6cbc1aeb48a93b780
joaobaptista03 <joaopedromotabaptista2003@gmail.com>
2023-04-27 21:24:03 +0100

    ex1 g6

--- SO/G6 - Dups/Exercício 1/Exercício 1.c
+++ SO/G6 - Dups/Exercício 1/Exercício 1.c
@@ -9,34 +9,32 @@ int main(int argc, char **argv) {
             write(2, "Error opening /etc/passwd.\n", 28);
             exit(EXIT_FAILURE);
         }
+    if (dup2(file_stdin, 0) < 0) {
+        write(2, "Error redirecting stdin descriptor.\n", 37);
+        exit(EXIT_FAILURE);
+    }
+
     int file_stdout = open("saida.txt", O_WRONLY | O_CREAT | O_TRUNC, 0666);
         if (file_stdout < 0) {
             write(2, "Error opening saida.txt.\n", 26);
             exit(EXIT_FAILURE);
         }
+    if (dup2(file_stdout, 1) < 0) {
+            write(2, "Error redirecting stdout descriptor.\n", 38);
+            exit(EXIT_FAILURE);
+    }
+
     int file_stderror = open("erros.txt", O_WRONLY | O_CREAT | O_TRUNC, 0666);
         if (file_stderror < 0) {
             write(2, "Error opening erros.txt.\n", 26);
             exit(EXIT_FAILURE);
         }
-
-    if (dup2(0, file_stdin) < 0) {
-            write(2, "Error redirecting stdin descriptor.\n", 37);
-            exit(EXIT_FAILURE);
-        }
-
-    if (dup2(1, file_stdout) < 0) {
-            write(2, "Error redirecting stdout descriptor.\n", 38);
-            exit(EXIT_FAILURE);
-        }
-
-    if (dup2(2, file_stderror) < 0) {
+    if (dup2(file_stderror, 2) < 0) {
             write(2, "Error redirecting stderror descriptor.\n", 40);
             exit(EXIT_FAILURE);
-        }
+    }
 
-    write(1, "terminei\n", 10);
-    fprintf(stderr, "Esta é uma mensagem de erro\n");
+    write(1, "terminei", 9);
 
     close(file_stdin);
     close(file_stdout);
